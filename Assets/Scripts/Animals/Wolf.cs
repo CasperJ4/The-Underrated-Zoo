d7c9e2f6eac5a398abca84a6ef4e7238f223358f
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-04-13 18:06:21 +0200

    Added script to the wolf

--- Assets/Scripts/Animals/Wolf.cs
+++ Assets/Scripts/Animals/Wolf.cs
@@ -6,6 +6,19 @@ namespace Lesson_6.Animals
 {
     public class Wolf : HostileAnimal
     {
+        private Vector3 _startPosition; // The starting position of the object
+        private float _angle = 0f; // The current angle of _startPosition
 
+        void Start()
+        {
+            _startPosition = transform.position;
+        }
+
+        protected void Update()
+        {
+            Vector3 end = new Vector3(Random.Range(0f, 10f), Random.Range(0f, 10f), Random.Range(0f, 10f));
+
+            transform.position = Vector3.Lerp(transform.position, end, Mathf.PingPong(Time.time * speed, 1.0f));
+        }
     }
 }
